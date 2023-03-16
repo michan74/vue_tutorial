0245ad40b13c004d8e2b0672cb234d65a841fe3a
e-sugiura <e-sugiura@wwwave.jp>
2023-03-16 09:12:04 +0900

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <EmployeeTable :employees="employees"></EmployeeTable>
 </template>
 
 <script>
+import EmployeeTable from '@/components/EmployeeTable.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
 }
 </script>
 
